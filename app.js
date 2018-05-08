be5b4febda0d2dc0c9a65715c2193ed96dca245f
Student <Student>
2018-05-08 12:22:24 +0300

    added changes

--- app.js
+++ app.js
@@ -1,28 +1,20 @@
-const form = document.querySelector("form")
-const taskInput = document.querySelector("input[name=taskName]")
-const dateInput = document.querySelector("input[name=taskDate") 
-const ul =document.querySelector("ul")
+var app = new Vue({
+  el: '#app',
+  data: {
 
-form.onsubmit = function(e) {
-	e.preventDefault()
+  todos: [
+  'my first day at work',
+  'my second day at work'
 
-	let taskTittle = taskInput.value
-	let taskDate = dateInput.value
-	
-	const li = document.createElement("li")
-	li.textContent = taskTittle
-	li.classList.add("list-group-item")
-	li.classList.add("d-flex")
-	li.classList.add("justify-content-between")
-	li.classList.add("align-items-centre")
+  ],
 
+  item: 'To do item'
 
-	const span = document.createElement("span")
-	span.textContent = taskDate
-	span.classList.add("badge")
-	span.classList.add("badge-primary")
-	span.classList.add("badge-pill")
-
-	li.appendChild(span)
-	ul.appendChild(li)
-}
+  },
+  methods:{
+  	addTodo(){
+  		this.todos.push(this.item)
+  		this.item ='';
+  	}
+  }
+})
